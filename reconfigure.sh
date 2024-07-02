b3ed660d0576e66e06fdafb0bb20d72c9af807cb
Charulata Sree Pokala <charulata014@gmail.com>
2024-07-02 16:36:53 -0400

    adding CI/CD

--- reconfigure.sh
+++ reconfigure.sh
@@ -1,7 +1,23 @@
+#!/bin/bash
+
+echo "Cleaning pub cache..."
 rm -rf ~/.pub-cache/
-flutter pub cache repair 
-find . -type f -name '*.g.dart' -print -delete && find . -type f -name '*.freezed.dart' -print -delete && find . -type f -name
+
+echo "Repairing pub cache..."
+flutter pub cache repair
+
+echo "Deleting generated files..."
+find . -type f -name '*.g.dart' -print -delete
+find . -type f -name '*.freezed.dart' -print -delete
+
+echo "Running flutter clean..."
 flutter clean
+
+echo "Removing pubspec.lock..."
 rm pubspec.lock
+
+echo "Getting pub packages..."
 flutter pub get
-flutter packages pub run build_runner build --delete-conflicting-outputs 
+
+echo "Running build_runner..."
+flutter packages pub run build_runner build --delete-conflicting-outputs
